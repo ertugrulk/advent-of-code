250a7ac168a16ec464dc07330406e7ec69444e37
ErtugrulK <athlon6000@gmail.com>
2020-12-08 22:36:06 +0100

    day8 - part 2

--- day8.swift
+++ day8.swift
@@ -1,25 +1,48 @@
 import Foundation
 
-var data = readInput(day: 8).map { Instruction(data: $0) }
-part1()
+let instructions = readInput(day: 8).map { Instruction(data: $0) }
+findAccValueAtLoopsEnd()
+fixInfiniteLoop()
 
-func part1() {
+func findAccValueAtLoopsEnd() {
+    let result = executeInstructions(instructions: instructions)
+    print("Acc value at the end of the infinite loop is \(result.lastAccValue)")
+}
+
+
+func fixInfiniteLoop() {
+    for i in 0..<instructions.count {
+        var instructionsToTry = instructions
+        if (["jmp","nop"].contains(instructionsToTry[i].op)) {
+            instructionsToTry[i].revertOperation()
+            let result = executeInstructions(instructions: instructionsToTry)
+            if (!result.isInfiniteLoop) {
+                print("Acc value after fixing infinite loop is \(result.lastAccValue)")
+                return
+            }
+        }
+    }
+}
+
+
+func executeInstructions(instructions: [Instruction]) -> (isInfiniteLoop: Bool, lastAccValue: Int) {
+    var data = instructions
+    let instructionCount = instructions.count
     var acc = 0
     var i = -1
     repeat {
         i += 1
         if (data[i].executed) {
-            print("Accumulator value before the second run is \(acc)")
-            return
+            return (true, acc)
         } else {
             data[i].execute(acc: &acc, offset: &i)
         }
-    } while true
+    } while i < instructionCount - 1
+    return (false, acc)
 }
 
-
 struct Instruction {
-    let op: String
+    var op: String
     let value: Int
     var executed: Bool
 }
@@ -44,8 +67,8 @@ extension Instruction{
         self.executed = true
     }
 
-    mutating func markAsExecuted() {
-        self.executed = true
+    mutating func revertOperation() {
+        self.op = self.op == "nop" ? "jmp" : "nop"
     }
 }
 
