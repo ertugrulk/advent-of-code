550d6e913f9f6475be0b758d60f96a6a959d2116
ErtugrulK <athlon6000@gmail.com>
2020-12-10 08:29:19 +0100

    2020 day 10 - part 1

---
+++ 2020/day10.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+var data = readInput(day: 10).map { Int($0)! }
+part1()
+
+func part1() {
+    let sortedAdapters = data.sorted()
+    var diffByOne = 1
+    var diffByThree = 1
+    for i in 1..<sortedAdapters.count {
+        let diff = sortedAdapters[i] - sortedAdapters[i - 1]
+        if diff == 1 {
+            diffByOne += 1
+        }
+        else if diff == 3 {
+            diffByThree += 1
+        }
+    }
+    print("1: \(diffByOne), 3: \(diffByThree)")
+}
+
+
+func readInput(day: Int) -> [String]{
+    let path = "inputs/day\(day)-input.txt"
+    let content = try! String(contentsOfFile:path)
+    return content.components(separatedBy: CharacterSet.newlines)
+}
