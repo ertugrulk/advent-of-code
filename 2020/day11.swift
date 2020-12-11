2b053f5d21e5c6762918dcc5acbbc0e3a530a366
ErtugrulK <athlon6000@gmail.com>
2020-12-11 20:16:35 +0100

    day11 - part 2

--- 2020/day11.swift
+++ 2020/day11.swift
@@ -5,21 +5,33 @@ var data = readInput(day: 11).map { row in
         seat(rawValue: String(rowSeat))!
     }
  }
+
+
 part1()
-
-
-func printLayout(rows: [[seat]]) {
-    for row in rows {
-        let mappedRow = row.map { $0.rawValue }.reduce("", +)
-        print ( mappedRow )
-    }
-}
+part2()
 
 func part1() {
+    let occupiedSeatCount = occupiedSeatCountAfterStabilization(
+        ignoreFloorsWhenFindingAdjacentSeats: false, 
+        maximumOccupancyForEmptyingSeats: 4)
+    print("Part 1: \(occupiedSeatCount) seats are occupied")
+}
+
+func part2() {
+    let occupiedSeatCount = occupiedSeatCountAfterStabilization(
+        ignoreFloorsWhenFindingAdjacentSeats: true, 
+        maximumOccupancyForEmptyingSeats: 5)
+    print("Part 2: \(occupiedSeatCount) seats are occupied")
+}
+
+func occupiedSeatCountAfterStabilization(ignoreFloorsWhenFindingAdjacentSeats: Bool,  maximumOccupancyForEmptyingSeats: Int) -> Int {
     var lastLayout = data
     
     repeat {
-        let currentLayout = processSeatingLayout(rows: lastLayout)
+        let currentLayout = processSeatingLayout(rows: lastLayout, 
+        maximumOccupancyForEmptyingSeats: maximumOccupancyForEmptyingSeats,
+        ignoreFloorsWhenFindingAdjacentSeats: ignoreFloorsWhenFindingAdjacentSeats
+        )
         if (currentLayout == lastLayout) {
             break
         } else {
@@ -27,45 +39,77 @@ func part1() {
         }
     } while true
     
-    let occupiedSeatCount = lastLayout.map { row in
+    return lastLayout.map { row in
         row.filter { $0 == .occupied }.count
     }.reduce(0, +)
-    print("After processing layouts, \(occupiedSeatCount) seats are occupied")
 }
 
 func getAdjacentSeatsFromRow(row: [seat], column: Int, includeMiddleSeat: Bool) -> [seat?] {
     return [row[optional: column - 1], includeMiddleSeat ? row[optional: column] : nil, row[optional: column + 1]]
 }
 
-func findAdjacentSeats(layout: [[seat]], row: Int, column: Int) -> [seat] {
-    let previousRow : [seat] = row >= 1 ? layout[row - 1] : []
-    let currentRow = layout[row]
-    let nextRow : [seat] = row < layout.count - 1 ? layout[row + 1] : []
-    let seats = getAdjacentSeatsFromRow(row: previousRow, column: column, includeMiddleSeat: true) +
-    getAdjacentSeatsFromRow(row: currentRow, column: column, includeMiddleSeat: false) +
-    getAdjacentSeatsFromRow(row: nextRow, column: column, includeMiddleSeat: true)  
-    return seats.compactMap { $0 }
+func getFirstAdjacentSeat(layout: [[seat]], row: Int, column: Int, direction: direction, ignoreFloors: Bool) -> seat? {
+    var x = column
+    var y = row
+    repeat {
+        switch(direction){
+            case .down: 
+                y += 1
+            case .downleft: 
+                x -= 1
+                y += 1
+            case .downright:
+                x += 1
+                y += 1
+            case .left:
+                x -= 1
+            case .right: 
+                x += 1
+            case .up:
+                y -= 1
+            case .upleft: 
+                x -= 1
+                y -= 1
+            case .upright:
+                x += 1
+                y -= 1    
+        }
+        guard let row = layout[optional: y] else { return nil }
+        guard let seat = row[optional: x] else { return nil }
+        if ignoreFloors && seat == .floor {
+            continue
+        } else {
+            return seat
+        }
+    } while true
+}
+
+func findAdjacentSeats(layout: [[seat]], row: Int, column: Int, ignoreFloors: Bool) -> [seat] {
+    return direction.allCases.compactMap { getFirstAdjacentSeat(layout: layout, row: row, column: column, direction: $0, ignoreFloors: ignoreFloors) }
 }
 
 func isSeatOccupiable(adjacentSeats: [seat]) -> Bool{
     return adjacentSeats.allSatisfy { $0 != .occupied }
 }
 
-func canSeatBeEmptied(adjacentSeats: [seat]) -> Bool {
-    return adjacentSeats.filter { $0 == .occupied }.count >= 4
+func canSeatBeEmptied(adjacentSeats: [seat], maximumOccupancy: Int) -> Bool {
+    return adjacentSeats.filter { $0 == .occupied }.count >= maximumOccupancy
 }
 
-func processSeatingLayout(rows: [[seat]] ) -> [[seat]] {
+func processSeatingLayout(rows: [[seat]], 
+    maximumOccupancyForEmptyingSeats: Int,
+    ignoreFloorsWhenFindingAdjacentSeats: Bool) -> [[seat]] {
     var result : [[seat]] = []
     var processedRow : [seat]
-    for x in 0..<rows.count {
+    for row in 0..<rows.count {
         processedRow = []
-        for y in 0..<rows[x].count {
-            var seatState = rows[x][y]
-            let adjacentSeats = findAdjacentSeats(layout: rows, row: x, column: y)
+        for column in 0..<rows[row].count {
+            var seatState = rows[row][column]
+            let adjacentSeats = findAdjacentSeats(layout: rows, row: row, column: column, ignoreFloors: ignoreFloorsWhenFindingAdjacentSeats)
             if (seatState == .empty && isSeatOccupiable(adjacentSeats: adjacentSeats)) {
                 seatState = seat.occupied
-            } else if (seatState == .occupied && canSeatBeEmptied(adjacentSeats: adjacentSeats)) {
+            } else if (seatState == .occupied && canSeatBeEmptied(adjacentSeats: adjacentSeats,
+             maximumOccupancy: maximumOccupancyForEmptyingSeats)) {
                 seatState = seat.empty
             } 
             processedRow.append(seatState)
@@ -79,6 +123,17 @@ enum seat : String {
     case occupied = "#"
     case empty = "L"
     case floor = "."
+}
+
+enum direction : CaseIterable {
+    case up
+    case down
+    case left
+    case right
+    case upleft
+    case upright
+    case downleft
+    case downright
 }
 
 func readInput(day: Int) -> [String]{
